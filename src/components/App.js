12fd580ab7172a7e753f4609e291ab0879e6945a
LoneHippie <lonehippie.music@gmail.com>
2021-04-13 20:34:11 +0300

    aaaaahhhhh

--- src/components/App.js
+++ src/components/App.js
@@ -14,7 +14,7 @@ import Navbar from './Navbar';
     Notes: 
 
     7/4/21 - Make createdBy[0] refresh when user changes usernames
-    
+
 */////////
 
 const App = () => {
@@ -28,7 +28,7 @@ const App = () => {
     //on load check for valid jwt and retrieve user data
     useEffect(() => {
         fetch('https://drinkdex.herokuapp.com/api/v1/users/me', {
-            credentials: 'same-origin'
+            credentials: 'include'
         })
             .then(res => res.ok ? res.json() : res)
             .then(resJson => {
@@ -48,7 +48,7 @@ const App = () => {
 
     async function refreshUser() {
         fetch('https://drinkdex.herokuapp.com/api/v1/users/me', {
-            credentials: 'same-origin' //THIS IS HOW YOU DETECT HTTPONLY COOKIE, FUCK YEAH
+            credentials: 'include' //THIS IS HOW YOU DETECT HTTPONLY COOKIE, FUCK YEAH
         })
             .then(res => res.ok ? res.json() : res)
             .then(resJson => {
